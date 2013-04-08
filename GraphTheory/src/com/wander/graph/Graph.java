31676567c22ba714106fcfd2aba62cb70990618f
蒋建亮 <jianliang.j@gmail.com>
2013-04-08 09:11:27 +0800

    project GraphTheory added

---
+++ GraphTheory/src/com/wander/graph/Graph.java
@@ -0,0 +1,24 @@
+package com.wander.graph;
+/**
+ * 
+ * @author wander
+ *
+ */
+public class Graph {
+	
+	public Graph(){
+		
+	}
+	/**
+	 * breadth first search
+	 */
+	public void BFS(){
+		
+	}
+	/**
+	 * depth first search
+	 */
+	public void DFS(){
+		
+	}
+}
